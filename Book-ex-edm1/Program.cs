1d5074b621812efeb475657fb3025603527aa106
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-10-30 18:18:20 +0100

    some changes

---
+++ Book-ex-edm1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_ex_edm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QAuthors();
+        }
+ private static void QAuthors()
+    {
+        using (var context = new BooksEntities())
+        {
+            var authors = context.Authors;
+            foreach(var author in authors)
+            {
+                Console.WriteLine("Author name {0} och author id {1}", author.FirstName.Trim(),
+                                       author.AuthorID);
+            }
+            Console.WriteLine("Press enter");
+            Console.ReadLine();
+        }
+    }
+    }
+   
+}
---
+++ book-exampl-1-edm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_exampl_1_edm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    private static void QueryContacts()
+    {
+        using ( var context = new )
+    }
+}
