f6c3b5680789a4dc8f107f69b7fa08196fb64ce7
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-10-30 19:30:35 +0100

    helper class

--- lab3-1/Program.cs
+++ lab3-1/Program.cs
@@ -10,6 +10,24 @@ namespace lab3_1
     {
         static void Main(string[] args)
         {
+            //helper static class
+            Console.WriteLine("Skriv en tal: ");
+            float f = Hjalp.StringToFloat(Console.ReadLine());
+            Console.WriteLine(f * 12);
+            Console.ReadKey();
+        }
+
+    }
+    public static class Hjalp
+    {
+        public static float StringToFloat(string valueString)
+        {
+            float output;
+            bool isString = float.TryParse(valueString, out output);
+            if (isString)
+                return output;
+            else
+                return 0;
         }
     }
 }
