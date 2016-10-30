8b283d00343892812491dfe2b43f893b676773fe
Dalius Pamparas <dalius.pamparas@gmail.com>
2016-10-30 19:41:46 +0100

    extension method class

--- lab3-1/Program.cs
+++ lab3-1/Program.cs
@@ -11,9 +11,14 @@ namespace lab3_1
         static void Main(string[] args)
         {
             //helper static class
-            Console.WriteLine("Skriv en tal: ");
+            Console.WriteLine("Skriv en tal(helper class): ");
             float f = Hjalp.StringToFloat(Console.ReadLine());
             Console.WriteLine(f * 12);
+
+            //extension class with this
+            Console.WriteLine("Skriv en tal(extention method): ");
+            float f1 = Console.ReadLine().ToFloat();
+            Console.WriteLine(f1 * 12);
             Console.ReadKey();
         }
 
@@ -21,6 +26,19 @@ namespace lab3_1
     public static class Hjalp
     {
         public static float StringToFloat(string valueString)
+        {
+            float output;
+            bool isString = float.TryParse(valueString, out output);
+            if (isString)
+                return output;
+            else
+                return 0;
+        }
+    }
+
+    public static class ExtentionMethodClass
+    {
+        public static float ToFloat(this string valueString)
         {
             float output;
             bool isString = float.TryParse(valueString, out output);
